4f4ff56a4b4fd942297b0b17085ccce891bb7f01
dcruvolo <davidruvolo51@gmail.com>
2022-06-18 15:14:16 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <TreeView
+      id="patientSampleTree"
+      title="Patient Tree"
+      :data="treedata.children"
+    />
+  </div>
+</template>
+
+<script>
+import TreeView from "./components/TreeView";
+import treedata from "./assets/data.json";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      treedata: treedata,
+    };
+  },
+  components: {
+    TreeView,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #252525;
+  box-sizing: padding-box;
+  padding: 1.5em;
+}
+</style>
